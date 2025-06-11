9870f25d4fb918ce5ff64cdb0245af35787516c3
josephus <otejerejosephus@gmail.com>
2025-06-11 16:44:24 +0100

    successfully completed the posts table

--- aggregation.go
+++ aggregation.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -27,20 +32,22 @@ func browse(s *state, cmd command) error {
 	if err != nil {
 		return fmt.Errorf("error getting posts: %v", err)
 	}
-	println("get here")
+	println("Posts:")
 
 	for _, post := range posts {
-		fmt.Println(post)
 		postStruct := reflect.ValueOf(post)
 		for i := range postStruct.NumField() {
 			field := postStruct.Type().Field(i)
-			fmt.Printf("Title: %s, Value: %v", field.Name, postStruct.Field(i).Interface())
+			fmt.Printf("Title: %s, Value: %v\n", field.Name, postStruct.Field(i).Interface())
 		}
+		println("--------")
+		println("")
 	}
 	return nil
 }
 
 func scrapeFeeds(s *state) error {
+
 	// fetch next feed id
 	fetchedFeed, err := s.db.GetNextFeedToFetch(context.Background())
 	if err != nil {
@@ -64,18 +71,6 @@ func scrapeFeeds(s *state) error {
 		return fmt.Errorf("error in getting feed: %v", err)
 	}
 
-	// store posts
-	postParams := CreatePostParams {
-	ID:          uuid.New(),
-	CreatedAt:   time.Time
-	UpdatedAt:   time.Time
-	Title:       string
-	Url:         string
-	Description: string
-	PublishedAt: time.Time
-	FeedID:      uuid.UUID
-}
-
 	// save post
 	newPost := database.CreatePostParams{
 		ID:          uuid.New(),
@@ -90,18 +85,14 @@ func scrapeFeeds(s *state) error {
 
 	post, err := s.db.CreatePost(context.Background(), newPost)
 	if err != nil {
-		return fmt.Errorf("error creating post: %v", err)
+		if strings.Contains(err.Error(), "duplicate key value") {
+			fmt.Println("posts updated")
+		} else {
+			log.Printf("error creating post: %v", err)
+		}
+	} else {
+		fmt.Printf("successfully created post: %s\n", post.Title)
 	}
 
-	fmt.Printf("successfully created post: %s", post.Title)
-
-	/*
-		// print the field name and its value
-		feedStruct := reflect.ValueOf(feed)
-		for i := range feedStruct.NumField() {
-			field := feedStruct.Type().Field(i)
-			fmt.Printf("Title: %s, Value: %v\n", field.Name, feedStruct.Field(i).Interface())
-		}
-	*/
 	return nil
 }
